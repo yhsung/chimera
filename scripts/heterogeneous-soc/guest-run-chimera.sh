121612cdeecf3f694873b8bf48aa6856dddd75b1
Yuehhsin Sung <yhsung0310@gmail.com>
2026-06-06 20:14:12 +0800

    feat: add TAP networking to MIPS guest (tap-mips, virtio-net-pci)

--- scripts/heterogeneous-soc/guest-run-chimera.sh
+++ scripts/heterogeneous-soc/guest-run-chimera.sh
@@ -23,4 +23,6 @@ exec "${qemu_bin}" \
     -device ivshmem-doorbell,chardev=ivshmem,vectors="${IVSHMEM_VECTORS}" \
     -drive file="${MIPS_DEBIAN_DISK}",format=qcow2,if=virtio \
     -virtfs local,path="${PINGPONG_DIR}",mount_tag="${PINGPONG_SHARE_TAG}",security_model=none,id="${PINGPONG_SHARE_TAG}" \
+    -netdev tap,id=net0,ifname=tap-mips,script=no,downscript=no \
+    -device virtio-net-pci,netdev=net0 \
     -nographic
